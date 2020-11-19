d2f4b8c226c10c28b49d6a1e0a3c0ed8dae6b472
bianqu <bianqu@www.bccto.me>
2020-11-19 21:59:54 +0800

    update: youtube-dl

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,263 +1,256 @@
-const Copyplugin = require('copy-webpack-plugin');
+const Copyplugin = require("copy-webpack-plugin");
 module.exports = {
-    title: 'Changero',
-    description: "技术只是工具，不是目的",
-    base: '', // base路径，用于将网站部署到非根目录下，在vue文件和md文件中通过$withBase函数访问
-    chainWebpack: (config, isServer) => {
-        const isProduction = process.env.NODE_ENV === "production";
+  title: "Changero",
+  description: "技术只是工具，不是目的",
+  base: "", // base路径，用于将网站部署到非根目录下，在vue文件和md文件中通过$withBase函数访问
+  chainWebpack: (config, isServer) => {
+    const isProduction = process.env.NODE_ENV === "production";
 
-        if (isProduction) {
-            config.output.publicPath("https://cdn.jsdelivr.net/gh/changero/blog@gh-pages/");
-        }
-        config.plugin('copy').use(Copyplugin, [
-            [
-                { from: './.vuepress/static', to: './static' },
-                { from: './.vuepress/public', to: '.' },
-            ] // from 是命令的执行路径，to是生成的文件夹路径
-        ]);
-    },
-    evergreen: true, // 禁止转译到ES5,并且不会添加IE的polyfill
-    markdown: {
-        lineNumbers: true
-    },
-    plugins: [
-        // "@vuepress/back-to-top", // reco中内置了
-        '@vuepress/medium-zoom', // A JavaScript library for zooming images like Medium
-    ],
-    theme: 'reco',
-    head: [
-        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
-    ],
-    themeConfig: {
-        startYear: '2019',
-        author: 'Changero',
-        huawei: false,
-        nav: [
-            { text: '首页', link: '/', icon: 'reco-home' },
-            { text: '分享', link: '/share/', icon: 'reco-up' },
-            { text: '读书', link: '/read/', icon: 'reco-search' },
-            {
-                text: '编程',
-                icon: 'reco-menu',
-                items: [
-                    { text: 'frontend', link: '/code/frontend/' },
-                    { text: 'node', link: '/code/node/' },
-                    { text: 'typescript', link: '/code/typescript/' },
-                    { text: 'react', link: '/code/react/' },
-                    { text: 'react-native', link: '/code/react-native/' },
-                    { text: 'vue', link: '/code/vue/' },
-                    { text: 'tools', link: '/code/tools/' },
-                    { text: 'rxjs', link: '/code/rxjs/' },
-                    { text: 'git', link: '/code/git/' },
-                    { text: 'docker', link: '/code/docker/' },
-                    { text: 'electron', link: '/code/electron/' },
-                    { text: 'mongodb', link: '/code/mongodb/' },
-                    { text: '加解密', link: '/code/encryption/' },
-                    { text: 'linux相关教程和笔记', link: '/code/linux/史上最牛的Linux视频教程' },
-                ]
-            },
-            {
-                text: 'TimeLine',
-                link: '/timeline/',
-                icon: 'reco-date'
-            },
-            {
-                text: '好有券',
-                link: 'http://quan.changero.win',
-                icon: 'reco-other'
-            },
-            {
-                // text: '📧 联系我',
-                text: '联系我',
-                link: 'mailto://changero@126.com',
-                icon: 'reco-message'
-            }
-        ],
-        sidebar: {
-            "/share/": [
-                {
-                    title: '分享',
-                    collapsable: false,
-                    children: [
-                        '/share/',
-                        'cookie、session和token.md',
-                        'tools.md',
-                        'how-to-create-github-pages.md',
-                        'node-commander.md',
-                        'partClub总结.md',
-                        'gant-design小结.md',
-                        '记又一次重装系统的历程.md',
-                        "破解wetool历程.md",
-                        "https证书.md",
-                        "VSCode插件自备.md",
-                        "部署Heroku.md",
-                        "croc.md",
-                        "chrome插件.md",
-                    ]
-                },
-                {
-                    title: "openwrt",
-                    collapsable: false,
-                    children: [
-                        "openwrt调教历程.md"
-                    ]
-                }
-
-            ],
-            '/read/': [
-                {
-                    title: '读书',
-                    collapsable: false,
-                    children: [
-                        '/read/',
-                        '学习的方法.md',
-                        "记录想说的话.md",
-                        "富爸爸穷爸爸.md",
-                        "《走吧，张小研》读后感.md",
-                    ]
-                }
-            ],
-            "/code/react/": [
-                {
-                    title: 'React学习之路',
-                    collapsable: false,
-                    children: [
-                        "/code/react/",
-                        "hooks.md",
-                        "stateless组件如何通过静态方法更新.md",
-                        "useCallBack和useMemo.md"
-                    ]
-                }
-            ],
-            "/code/react-native/": [
-                "/code/react-native/",
-                "mac搭建react-native开发环境.md"
-            ],
-            "/code/rxjs/": [
-                "/code/rxjs/",
-                {
-                    title: '学习心得',
-                    collapsable: false,
-                    children: [
-                        "creator.md",
-                        "pipeable.md",
-                    ]
-                },
-                {
-                    title: "别人的资源",
-                    collapsable: true,
-                    children: []
-                }
-            ],
-            "/code/frontend/": [
-                {
-                    title: '前端',
-                    collapsable: false,
-                    children: [
-                        "/code/frontend/",
-                        "理解Unicode与UTF-8.md",
-                        "babel7入门级指南",
-                        "两种函数组合方式.md",
-                        "理解commonJS模块加载以及循环依赖的问题.md",
-                        "IFC.md",
-                        "Date对象时区转化.md",
-                        "自定义标签.md",
-                        "确定页面rem大小的两种思路.md",
-                        "css/gulp-less处理antd中的less引用.md",
-                        "浏览器table布局策略.md",
-                        "Object禁止对象扩展的方法比较.md",
-                        "promise and generator.md",
-                        "创建scope包.md",
-                        "使用JS访问摄像头.md",
-                    ]
-                }
-            ],
-            "/code/node/": [
-                {
-                    title: "工具",
-                    collapsable: false,
-                    children: [
-                        "lerna.md",
-                        "yargs.md",
-                    ]
-                }
-            ],
-            "/code/typescript/": [
-                {
-                    title: 'typescript',
-                    collapsable: false,
-                    children: [
-                        "收集的部分问题及解决方法.md",
-                        "生成d.ts.md"
-                    ]
-                }
-            ],
-            "/code/git/": [
-                {
-                    title: "git经验分享",
-                    collapsable: false,
-                    children: [
-                        'git常用命令.md',
-                        'git撤销操作.md',
-                        "git子模块.md",
-                        "git代理设置.md"
-                    ]
-                }
-            ],
-            "/code/docker/": [
-                {
-                    title: "遇到过的问题",
-                    collapsable: false,
-                    children: [
-                        "ubuntu容器设置启动脚本.md"
-                    ]
-                }
-            ],
-            "/code/electron/": [
-                {
-                    title: "electron",
-                    collapsable: false,
-                    children: [
-                        "解决electron下载问题.md"
-                    ]
-                }
-            ],
-            "/code/linux/": [
-                {
-                    title: 'linux相关教程',
-                    collapsable: false,
-                    children: [
-                        "史上最牛的Linux视频教程.md"
-                    ]
-                }
-            ],
-            "/code/mongodb/": [
-                {
-                    title: 'mongodb',
-                    collapsable: false,
-                    children: [
-                        "基础命令.md"
-                    ]
-                }
-            ],
-            "/code/encryption/": [
-                {
-                    title: '加解密',
-                    collapsable: false,
-                    children: [
-                        "basic_encryption.md"
-                    ]
-                }
-            ]
-        },
-        lastUpdated: '最后更新于',
-        blogConfig: {
-            category: {
-                location: 2,
-            },
-            tag: {
-                location: 3,     // 在导航栏菜单中所占的位置，默认3
-            }
-        },
+    if (isProduction) {
+      config.output.publicPath(
+        "https://cdn.jsdelivr.net/gh/changero/blog@gh-pages/"
+      );
     }
+    config.plugin("copy").use(Copyplugin, [
+      [
+        { from: "./.vuepress/static", to: "./static" },
+        { from: "./.vuepress/public", to: "." },
+      ], // from 是命令的执行路径，to是生成的文件夹路径
+    ]);
+  },
+  evergreen: true, // 禁止转译到ES5,并且不会添加IE的polyfill
+  markdown: {
+    lineNumbers: true,
+  },
+  plugins: [
+    // "@vuepress/back-to-top", // reco中内置了
+    "@vuepress/medium-zoom", // A JavaScript library for zooming images like Medium
+  ],
+  theme: "reco",
+  head: [
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
+  ],
+  themeConfig: {
+    startYear: "2019",
+    author: "Changero",
+    huawei: false,
+    nav: [
+      { text: "首页", link: "/", icon: "reco-home" },
+      { text: "分享", link: "/share/", icon: "reco-up" },
+      { text: "读书", link: "/read/", icon: "reco-search" },
+      {
+        text: "编程",
+        icon: "reco-menu",
+        items: [
+          { text: "frontend", link: "/code/frontend/" },
+          { text: "node", link: "/code/node/" },
+          { text: "typescript", link: "/code/typescript/" },
+          { text: "react", link: "/code/react/" },
+          { text: "react-native", link: "/code/react-native/" },
+          { text: "vue", link: "/code/vue/" },
+          { text: "tools", link: "/code/tools/" },
+          { text: "rxjs", link: "/code/rxjs/" },
+          { text: "git", link: "/code/git/" },
+          { text: "docker", link: "/code/docker/" },
+          { text: "electron", link: "/code/electron/" },
+          { text: "mongodb", link: "/code/mongodb/" },
+          { text: "加解密", link: "/code/encryption/" },
+          {
+            text: "linux相关教程和笔记",
+            link: "/code/linux/史上最牛的Linux视频教程",
+          },
+        ],
+      },
+      {
+        text: "TimeLine",
+        link: "/timeline/",
+        icon: "reco-date",
+      },
+      {
+        text: "好有券",
+        link: "http://quan.changero.win",
+        icon: "reco-other",
+      },
+      {
+        // text: '📧 联系我',
+        text: "联系我",
+        link: "mailto://changero@126.com",
+        icon: "reco-message",
+      },
+    ],
+    sidebar: {
+      "/share/": [
+        {
+          title: "分享",
+          collapsable: false,
+          children: [
+            "/share/",
+            "cookie、session和token.md",
+            "tools.md",
+            "how-to-create-github-pages.md",
+            "node-commander.md",
+            "partClub总结.md",
+            "gant-design小结.md",
+            "记又一次重装系统的历程.md",
+            "破解wetool历程.md",
+            "https证书.md",
+            "VSCode插件自备.md",
+            "部署Heroku.md",
+            "chrome插件.md",
+          ],
+        },
+        {
+          title: "工具",
+          collapsable: false,
+          children: ["tools/youtube-dl.md", "tools/croc"],
+        },
+        {
+          title: "openwrt",
+          collapsable: false,
+          children: ["openwrt调教历程.md"],
+        },
+      ],
+      "/read/": [
+        {
+          title: "读书",
+          collapsable: false,
+          children: [
+            "/read/",
+            "学习的方法.md",
+            "记录想说的话.md",
+            "富爸爸穷爸爸.md",
+            "《走吧，张小研》读后感.md",
+          ],
+        },
+      ],
+      "/code/react/": [
+        {
+          title: "React学习之路",
+          collapsable: false,
+          children: [
+            "/code/react/",
+            "hooks.md",
+            "stateless组件如何通过静态方法更新.md",
+            "useCallBack和useMemo.md",
+          ],
+        },
+      ],
+      "/code/react-native/": [
+        "/code/react-native/",
+        "mac搭建react-native开发环境.md",
+      ],
+      "/code/rxjs/": [
+        "/code/rxjs/",
+        {
+          title: "学习心得",
+          collapsable: false,
+          children: ["creator.md", "pipeable.md"],
+        },
+        {
+          title: "别人的资源",
+          collapsable: true,
+          children: [],
+        },
+      ],
+      "/code/frontend/": [
+        {
+          title: "前端",
+          collapsable: false,
+          children: [
+            "/code/frontend/",
+            "理解Unicode与UTF-8.md",
+            "babel7入门级指南",
+            "两种函数组合方式.md",
+            "理解commonJS模块加载以及循环依赖的问题.md",
+            "IFC.md",
+            "Date对象时区转化.md",
+            "自定义标签.md",
+            "确定页面rem大小的两种思路.md",
+            "css/gulp-less处理antd中的less引用.md",
+            "浏览器table布局策略.md",
+            "Object禁止对象扩展的方法比较.md",
+            "promise and generator.md",
+            "创建scope包.md",
+            "使用JS访问摄像头.md",
+          ],
+        },
+      ],
+      "/code/node/": [
+        {
+          title: "工具",
+          collapsable: false,
+          children: ["lerna.md", "yargs.md"],
+        },
+      ],
+      "/code/typescript/": [
+        {
+          title: "typescript",
+          collapsable: false,
+          children: ["收集的部分问题及解决方法.md", "生成d.ts.md"],
+        },
+      ],
+      "/code/git/": [
+        {
+          title: "git经验分享",
+          collapsable: false,
+          children: [
+            "git常用命令.md",
+            "git撤销操作.md",
+            "git子模块.md",
+            "git代理设置.md",
+          ],
+        },
+      ],
+      "/code/docker/": [
+        {
+          title: "遇到过的问题",
+          collapsable: false,
+          children: ["ubuntu容器设置启动脚本.md"],
+        },
+      ],
+      "/code/electron/": [
+        {
+          title: "electron",
+          collapsable: false,
+          children: ["解决electron下载问题.md"],
+        },
+      ],
+      "/code/linux/": [
+        {
+          title: "linux相关教程",
+          collapsable: false,
+          children: ["史上最牛的Linux视频教程.md"],
+        },
+      ],
+      "/code/mongodb/": [
+        {
+          title: "mongodb",
+          collapsable: false,
+          children: ["基础命令.md"],
+        },
+      ],
+      "/code/encryption/": [
+        {
+          title: "加解密",
+          collapsable: false,
+          children: ["basic_encryption.md"],
+        },
+      ],
+    },
+    lastUpdated: "最后更新于",
+    blogConfig: {
+      category: {
+        location: 2,
+      },
+      tag: {
+        location: 3, // 在导航栏菜单中所占的位置，默认3
+      },
+    },
+  },
 };
---
+++ share/tools/youtube-dl.md
@@ -0,0 +1,88 @@
+---
+title: youtube-dl使用指南
+date: 2020-11-19
+categories:
+  - tool
+---
+
+## 安装
+
+youtube-dl 的运行依赖 python
+
+- UNIX 系统
+
+> sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+> sudo chmod a+rx /usr/local/bin/youtube-dl
+
+或者
+
+> sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+> sudo chmod a+rx /usr/local/bin/youtube-dl
+
+- windows
+
+[下载链接](https://yt-dl.org/latest/youtube-dl.exe)，然后加到环境变量中。
+
+- pip
+
+通过 pip 安装:
+
+> sudo -H pip install --upgrade youtube-dl
+
+- macOS
+
+> brew install youtube-dl
+
+或者通过 macport 安装
+
+> sudo port install youtube-dl
+
+- ubuntu
+  > apt-get install youtube-dl
+
+## 使用简介
+
+> youtube-dl https://www.youtube.com/watch?v=eTFPMxqod2I
+
+- 使用代理
+
+  > youtube-dl https://www.youtube.com/watch?v=eTFPMxqod2I --proxy 127.0.0.1:1080
+
+- 指定文件名
+
+  > youtube-dl -o 'yd-video' [url]
+
+- 下载多个文件
+
+  > youtube-dl [url1][url2]
+
+      可以将它们全部放在文本文件中，并将其作为参数传递给Youtube-dl
+      > youtube-dl -a url.txt
+
+- 仅下载音频
+
+  > youtube-dl -x --audio-format mp3 [url]
+
+- 查看格式，能够得到格式代码
+
+  > youtube-dl -F [url]
+
+      下载特定格式
+      > youtube-dl -f 格式代码 [url]
+
+- 按格式下载
+
+  > youtube-dl --format mp4 [url]
+
+- 下载列表
+  > youtube-dl --download-archive archive.txt [playlist]
+
+## 配置文件
+
+> sudo touch /etc/youtube-dl.conf
+
+> vim /etc/youtube-dl.conf
+
+--proxy 127.0.0.1:1080
+
+-o ~/download
